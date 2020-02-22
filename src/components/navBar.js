bc198aaa8dc2fd9e47bf1b10857d42c073d6951e
Hussian Shaik <hussianshaik162@gmail.com>
2020-02-22 17:19:56 +0530

    Added useContext hooks

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,26 +1,19 @@
-import React,{Component} from 'react';
+import React,{Component,useContext} from 'react';
 import {ThemeContext} from '../context/ThemeContext';
 
-class NavBar extends Component{
-    render(){
-        return(
-            <ThemeContext.Consumer>{(context)=>{
-                const { isLightTheme, light, dark } = context;
-                const theme = isLightTheme? light: dark
-                return(
-                    <nav style={{background: theme.ui, color: theme.syntax}}>
-                        <h1>Context App</h1>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Contact</li>
-                        </ul>
-                    </nav>
-                )
-            }}
-            </ThemeContext.Consumer>
-        )
-    }
+const NavBar = ()=>{
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme? light: dark
+    return(
+        <nav style={{background: theme.ui, color: theme.syntax}}>
+            <h1>Context App</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default NavBar;
